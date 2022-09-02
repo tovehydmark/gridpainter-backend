afd5268a2ddf8706f0fd6d742a5843ae067a7a70
tovehydmark <tovehydmark@gmail.com>
2022-09-02 15:01:12 +0200

    jh

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const io = new Server(server, {
       'https://tovehydmark.github.io/gridpainter-frontend',
     ],
     methods: ['GET', 'POST'],
-    // credentials: true,
+    credentials: true,
   },
 });
 
